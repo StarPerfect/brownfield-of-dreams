da52da56c61611acba81a4a55d1c9a4c9c35be99
Andrew Johnson <manilda7@gmail.com>
2019-10-10 13:10:33 -0600

    Adds testing for following feature in dashboard
    Co-Authored-By: Corina Allen <starperfect@gmail.com>

---
+++ spec/features/user/user_can_see_their_github_following_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "User Following Section" do
+  describe "As a logged in user, when I visit '/dashboard'" do
+    it "I see a section 'Following' and underneath I see a list of Github users I follow" do
+      json_response = File.open("./fixtures/github_repo.json")
+      stub_request(:get, "https://api.github.com/user/repos").to_return(status: 200, body: json_response)
+      followers_response = File.open("./fixtures/github_followers.json")
+      stub_request(:get, "https://api.github.com/user/followers").to_return(status: 200, body: followers_response)
+      following_response = File.open("./fixtures/github_following.json")
+      stub_request(:get, "https://api.github.com/user/following").to_return(status: 200, body: following_response)
+
+      user = User.create!(first_name: 'Bob', last_name: 'Ross', password: 'HappyTree', github_token: '12345', email: 'Mr.Ross@gmail.com')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content('Following')
+
+      within(first('.following')) do
+        expect(page).to have_css('.login')
+        expect(page).to have_link('Martsy')
+      end
+    end
+  end
+end
+
+
+
+# As a logged in user
+# When I visit /dashboard
+# Then I should see a section for "Github"
+# And under that section I should see another section titled "Following"
+# And I should see list of users I follow with their handles linking to their Github profile
