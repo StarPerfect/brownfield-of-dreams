fda9ed640625c23d262ce37dea47357b9f2e7769
Andrew Johnson <manilda7@gmail.com>
2019-10-10 09:53:02 -0600

    Adds some additional configuration to vcr and webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,7 +17,10 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data('<YOUTUBE_API_KEY>') { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<USER_GITHUB_API_KEY>") {ENV['USER_GITHUB_API_KEY']}
+  config.filter_sensitive_data("<GITHUB_KEY>") {ENV['GITHUB_KEY']}
+  config.filter_sensitive_data("<GITHUB_SECRET>") {ENV['GITHUB_SECRET']}
 end
 
 ActiveRecord::Migration.maintain_test_schema!
