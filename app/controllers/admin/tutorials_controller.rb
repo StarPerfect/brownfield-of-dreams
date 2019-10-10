8775a2299dce402dd53c5ea1f0c1917f944e2e2c
Corina Allen <StarPerfect@gmail.com>
2019-10-10 13:16:08 -0600

    WIP for admin to create new tutorial

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -5,7 +5,17 @@ class Admin::TutorialsController < Admin::BaseController
     @tutorial = Tutorial.find(params[:id])
   end
 
-  def create; end
+  def create
+    binding.pry
+    # <ActionController::Parameters {"utf8"=>"✓", "authenticity_token"=>"uOQ6TsJ7SZ478E96BHHCwGNqxMAwpP4XoRPCO8UBmntmtgJxinOWEnI4sSWkBO2rEfxfXH5AyAzbjF/xpqEPKQ==",
+    #    "tutorial"=>{"title"=>"Hello",
+    #      "description"=>"Something",
+    #      "thumbnail"=>"https://www.youtube.com/watch?v=AXpuu8IXdm4&list=PLOU8Lmsz4j5Hw1gDqwUgcE20n8Q0WR4er&index=7&t=4s"
+    #      }, "commit"=>"Save",
+    #      "controller"=>"admin/tutorials",
+    #      "action"=>"create"
+    #      } permitted: false>
+  end
 
   def new
     @tutorial = Tutorial.new
---
+++ spec/features/admin/new_tutorial_spec.rb
@@ -0,0 +1,32 @@
+# When I visit '/admin/tutorials/new'
+# And I fill in 'title' with a meaningful name
+# And I fill in 'description' with a some content
+# And I fill in 'thumbnail' with a valid YouTube thumbnail
+# And I click on 'Save'
+# Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
+# And I should see a flash message that says "Successfully created tutorial."
+require 'rails_helper'
+
+describe 'Add New Tutorial by Admin' do
+  describe 'As an admin, when I visit the new tutorial path' do
+    before :each do
+      admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    end
+
+    it 'Can submit details and create a new tutorial' do
+      visit new_admin_tutorial_path
+
+      fill_in 'Title', with: 'A Meaningful Name'
+      fill_in 'Description', with: 'Some Content'
+      fill_in 'Thumbnail', with: 'Valid YouTube Thumbnail'
+
+      click_on 'Save'
+
+      tutorial = Tutorial.last
+binding.pry
+      expect(current_path).to eq("/tutorials/#{tutorial.id}")
+      expect(page).to have_content('Successfully created tutorial.')
+    end
+  end
+end
