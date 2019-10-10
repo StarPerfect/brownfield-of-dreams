6015cc8840101d7dab34ade8097e033c3250bef3
Corina Allen <StarPerfect@gmail.com>
2019-10-10 10:50:55 -0600

    Complete GitHub Repos functionality including Sad Path testing - all specs passing
    Co-Authored-By: Andrew Johnson <manilda7@gmail.com>

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,41 +1,16 @@
 class UserFacade
-  attr_reader :current_user
-
   def initialize(current_user)
     @current_user = current_user
   end
 
   def repos
-    conn = Faraday.new(url: "https://api.github.com/") do |f|
-      f.headers['Authorization'] =  "token " + ENV['github_token']
-      f.adapter Faraday.default_adapter
-    end
-
-    json_response = conn.get("/user/repos")
-
-    parsed_data = JSON.parse(json_response.body, symbolize_names: true)
-
-    parsed_data.map do |data|
+    service = GithubApiService.new
+    service.get_user_repos.map do |data|
       Repo.new(data)
-    end
+    end.take(5)
   end
 
+private
 
+  attr_reader :current_user
 end
-
-
-
-# def self.get_repos(username)
-#   json_response = conn.get("/#{username}/repos")
-#   parsed_data = JSON.data(json_response.body, symbolize_names: true)
-# end
-#
-# private
-#
-# def conn
-#   Faraday.new(
-#     url: 'https://api.github.com/users',
-#     headers: {'Accept' => 'application/vnd.github.v3+json',
-#               'Authorization' => ENV['github_token']},
-#   )
-# end
