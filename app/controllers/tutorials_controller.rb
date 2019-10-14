0f501d8c5aad8731c5464914133b385b501cc8e2
Andrew Johnson <manilda7@gmail.com>
2019-10-14 13:57:59 -0600

    Adds conditional to tutorials index view to check for current user and tutorial.classroom.

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class TutorialsController < ApplicationController
+  def index
+    @tutorials = Tutorial.all
+  end
+
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
