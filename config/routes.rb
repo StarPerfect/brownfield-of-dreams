b07b5542155d0807b9054bf53016f12412bc8e97
Corina Allen <StarPerfect@gmail.com>
2019-10-16 15:24:28 -0600

    Add TutorialLogin route for unlogged in users to bookmark videos
    Co-Authored-By: Andrew Johnson <Loomus@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/tutorial_login', to: 'sessions#bkmrklogin', as: :tutorial_login
   delete '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'users#show'
