29992fc807585e69efba9da207f39660e184e9e7
Andrew Johnson <manilda7@gmail.com>
2019-10-17 11:06:48 -0600

    Adds test for registered? method

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -13,4 +13,19 @@ describe GithubUser do
     expect(user.login).to eq('HeatherHerries')
     expect(user.html_url).to eq('https://github.com/HeatherHerries')
   end
+
+  describe 'instance methods' do
+    it '#registered?' do
+      attributes = { login: 'Joey Fatone',
+                   html_url: 'https://joeyfatone.com',
+                   id: '14723' }
+
+      @user = GithubUser.new(attributes)
+      create(:user, github_uid: '14723')
+      unregistered_user = GithubUser.new(id: '1738')
+
+      expect(@user.registered?).to eq(true)
+      expect(unregistered_user.registered?).to eq(false)
+    end
+  end
 end
