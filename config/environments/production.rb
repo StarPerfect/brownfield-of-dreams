550490f3465d4fba8b09189149d81a512fa64a77
Corina Allen <StarPerfect@gmail.com>
2019-10-17 08:55:08 -0600

    Add default url options for ActionMailer to config environtments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "personal_project_#{Rails.env}"
-
+  config.action_mailer.default_url_options = { :host => 'https://infinite-taiga-32566.herokuapp.com'}
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
 
