54ca433f38d2360b85e7cbaf3474a5ff2f7d8f0c
Andrew Johnson <manilda7@gmail.com>
2019-10-17 11:07:09 -0600

    Adds uid as an attribute and registered? method

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,8 +1,15 @@
 class GithubUser
-  attr_reader :login, :html_url
+  attr_reader :login,
+              :html_url,
+              :uid
 
   def initialize(data = {})
     @login = data[:login]
     @html_url = data[:html_url]
+    @uid = data[:id]
+  end
+
+  def registered?
+    !User.find_by(github_uid: @uid).nil?
   end
 end
